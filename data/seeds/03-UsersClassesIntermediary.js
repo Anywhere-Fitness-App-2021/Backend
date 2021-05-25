c8f3e17a4bc42ea442e62dad83fa1a52fe09c1d3
ahmedseragcodes <ahmedserag1@gmail.com>
2021-05-25 12:52:36 -0400

    got get request for User's Classes Working

--- data/seeds/03-UsersClassesIntermediary.js
+++ data/seeds/03-UsersClassesIntermediary.js
@@ -1,6 +1,8 @@
 exports.seed = function(knex) {
   return knex("UsersClassesIntermediary").insert([
-    {User_Id: 1, Class_Id_One: 1, Class_Id_Two: 2},
-    {User_Id: 2, Class_Id_One: 3, Class_Id_Two: 2},
+    {User_Id: 1, Class_Id: 1},
+    {User_Id: 1, Class_Id: 2},
+    {User_Id: 2, Class_Id: 3},
+    {User_Id: 2, Class_Id: 2},
   ])
 };
