611310bad24f7b6eb1165eb7cad7bee97068d5b7
Brett Miller <brettmiller@Bretts-MacBook-Air.local>
2021-05-25 02:56:07 -0700

    added some endpoints, updated db users to have a seperate id

--- api/users/users.router.js
+++ api/users/users.router.js
@@ -1,8 +1,109 @@
 const express = require("express");
 const Users = require("./users-model");
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+//const { jwtSecret } = 
 
 //ENDPOINTS
+
+//[GET] All Users
+
+router.get("/", (req, res, next) => {
+    Users.getAllUsers()
+    .then((allUsers) => {
+        res.status(200).json(allUsers);
+    })
+    .catch((err) => {
+        res.status(500).json({message: err.message});
+    })
+})
+
+//[GET] User by ID
+
+router.get("/:UserId", (req, res, next) => {
+    const {UserId} = req.params; 
+
+    if(UserId){
+        Users.getUserById(req.params.UserId)
+            .then(specificUser => {
+                res.status(200).json(specificUser)
+            })
+            .catch(err => {
+                res.status(500).json({message: err.message})
+            })
+    } else {
+        res.status(406).json({message: "User Id Required"})
+    }
+})
+
+//[POST] Create User
+//Needs Auth Code if Instructor - Not yet added
+//Most likely needs another Post method to create a password
+
+router.post("/", (req, res, next)=>{
+
+    const newUser = req.body;
+
+    if(newUser.UserId && newUser.Name){
+        if (typeof newUser.UserId === "number"){
+            Users.addUser(newUser)
+            .then((newestUser)=>{
+                res.status(200).json(newestUser);
+            })
+            .catch((err)=>{
+                res.status(500).json({message: err.message});
+            })
+        } else {
+            res.status(406).json({message: "UserId must be a number"});
+        }
+    } else {
+        res.status(406).json({message: "UserId and Name are required"});
+    }
+
+
+    
+})
+
+//[PUT] Update User By UserId
+
+router.put("/:UserId", (req, res, next)=>{
+
+    const updatedUser = req.body;
+
+    if(updatedUser.UserId && updatedUser.Name){
+        if (typeof updatedUser.UserId === "number"){
+            Users.updateUser(updatedUser)
+            .then((update)=>{
+                res.status(200).json(update);
+            })
+            .catch((err)=>{
+                res.status(500).json({message: err.message});
+            })
+        } else {
+            res.status(406).json({message: "UserId must be a number"});
+        }
+    } else {
+        res.status(406).json({message: "UserId and Name are required"});
+    }
+    
+})
+
+//[DELETE] User By UserId
+
+router.delete("/:UserId", (req, res, next)=>{
+    
+    const { UserId } = req.params;
+
+    Users.deleteuser(UserId)
+    .then((resolution)=>{
+        res.status(200).json(resolution);
+    })
+    .catch((err)=>{
+        res.status(500).json({message: err.message});
+    })
+
+})
 
 //[GET] Users Classes
 
--- data/seeds/01-Users.js
+++ data/seeds/01-Users.js
@@ -1,7 +1,7 @@
 exports.seed = function(knex) {
   return knex("Users").insert([
-    {username: "instructor", password: "1234", role: "instructor"},
-    {username: "instructor2", password: "1234", role: "instructor"},
-    {username: "client", password: "1234", role: "client"}
+    {UserId: 1, Username: "instructor", Password: "1234", Role: "instructor"},
+    {UserId: 2, Username: "instructor2", Password: "1234", Role: "instructor"},
+    {UserId: 3, Username: "client", Password: "1234", Role: "client"}
   ])
 };
